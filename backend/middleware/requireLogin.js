ed5a3ac771ae3146a8411ed587c0750c32ad2f3a
Gursahib Singh <aulakhg808@gmail.com>
2025-08-05 11:19:56 +0530

    require login

--- backend/middleware/requireLogin.js
+++ backend/middleware/requireLogin.js
@@ -1,3 +1,22 @@
-export default requireLogin = (req,res,next) => {}
+import jwt from "jsonwebtoken"
+import User from "../models/User.js"
 
-//verfication pending
+export default async function requireLogin(req,res,next) {
+
+
+    try {
+    const {authorization} = req.headers
+
+    if(!authorization){
+       return res.status(401).json({error:"You  must be login"})
+    }
+
+    const token = authorization.replace("Bearer " , "")
+    const payload =  jwt.verify(token,process.env.SECRETKEY)
+    const {_id} = payload
+    const user  =   await User.findById(_id)
+    console.log(user);2
+    } catch (error) {
+        return res.status(401).json({error:"you must be login"})
+    }
+}
