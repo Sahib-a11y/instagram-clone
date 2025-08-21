cc6428ea70b4612dcb5a7e8c7da0977aa085768b
Gursahib Singh <aulakhg808@gmail.com>
2025-08-21 14:15:52 +0530

    frontend\src\components\feed

---
+++ frontend/src/components/feed.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Post from "./Post";
+
+const Feed = ({posts,token}) => {
+    return(
+        <div className="space-y-6">
+            {posts.length > 0 ? (
+                posts.map(post => (
+                    <Post key = {post._id} post = {post} token={token} />
+                ))
+            ):(
+                <p className="text-center text-gray-500">You not posted posts </p>
+            )}
+        </div>
+    )
+}
+export default Feed
